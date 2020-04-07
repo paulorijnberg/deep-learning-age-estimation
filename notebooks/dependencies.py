45ca1dbc0e5a020b622c1f286433f7a2d1dd919b
paulorijnberg <paulo_rijnberg@hotmail.com>
2020-04-07 11:19:45 +0200

    created dependencies and general_functions module to easily have one file to keep track of libaries and created functions

---
+++ notebooks/dependencies.py
@@ -0,0 +1,50 @@
+# General libaries
+import pandas as pd
+import numpy as np
+import os
+import pickle
+
+# Preprocessing
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.utils import shuffle
+
+# Neural network related
+import keras
+from keras import models
+from keras.models import Model
+from keras.layers import Conv2D, MaxPooling2D, Dropout, Flatten, Dense, Activation, BatchNormalization, Input
+from keras import layers
+from keras.preprocessing.image import ImageDataGenerator # To create an image generator to create batches of images
+from keras.preprocessing import image # To change images to an np array AND visualize the image
+from keras import optimizers # to optimize
+from keras.models import load_model # Load model
+from keras.callbacks import ModelCheckpoint # To save best model
+
+from keras.utils.vis_utils import plot_model # To plot models
+import pydot # To plot models
+
+# Visualizing/visualizing activation of conv layers
+import matplotlib.pyplot as plt
+import cv2
+import matplotlib.image as mpimg
+
+# To clear ram
+from tensorflow.keras import backend as K
+K.clear_session()
+
+# To get information about ram
+import multiprocessing
+
+# Transfer Learning
+from keras.applications.resnet50 import ResNet50
+from keras.applications.resnet50 import preprocess_input, decode_predictions
+
+from keras.applications.vgg16 import VGG16
+from keras.applications.vgg16 import preprocess_input
+
+
+
+pd.set_option('max_colwidth', None)
+pd.set_option("display.max_rows", 100)
+
+np.seterr(divide='ignore', invalid='ignore')
---
+++ notebooks/general_functions.py
@@ -0,0 +1,177 @@
+# General libaries
+import pandas as pd
+import numpy as np
+import os
+import pickle
+
+# Preprocessing
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.utils import shuffle
+
+# Neural network related
+import keras
+from keras import models
+from keras.models import Model
+from keras.layers import Conv2D, MaxPooling2D, Dropout, Flatten, Dense, Activation, BatchNormalization, Input
+from keras import layers
+from keras.preprocessing.image import ImageDataGenerator # To create an image generator to create batches of images
+from keras.preprocessing import image # To change images to an np array AND visualize the image
+from keras import optimizers # to optimize
+from keras.models import load_model # Load model
+from keras.callbacks import ModelCheckpoint # To save best model
+
+from keras.utils.vis_utils import plot_model # To plot models
+import pydot # To plot models
+
+# Visualizing/visualizing activation of conv layers
+import matplotlib.pyplot as plt
+import cv2
+import matplotlib.image as mpimg
+
+# To clear ram
+from tensorflow.keras import backend as K
+K.clear_session()
+
+# To get information about ram
+import multiprocessing
+
+# Transfer Learning
+from keras.applications.resnet50 import ResNet50
+from keras.applications.resnet50 import preprocess_input, decode_predictions
+
+from keras.applications.vgg16 import VGG16
+from keras.applications.vgg16 import preprocess_input
+
+
+
+def load_dataset(filename):
+    """
+    This function loads the dataset.
+    """
+    with open(filename, 'rb') as input_file:
+        df = pickle.load(input_file)
+        
+    return df
+
+
+def save_history_model(history, filename, storage_location):
+    """
+    This function pickles the history of a model and stores it
+    at the storage_location with the given filename.
+    """
+    
+    # Pickle the result
+    location = os.path.join(storage_location, filename + '.pkl')
+    
+    with open(location, 'wb') as output_file:
+        pickle.dump(history, output_file)
+        
+def plot_history(history, x_as, y_as, title, line1='accuracy', line2='val_accuracy'):
+    """
+    This function plots two lines, which as default are accuracy
+    and val_accuracy.
+    """
+    
+    history = history.history
+    
+    plt.plot(range(1, len(history[line1]) + 1), history[line1])
+    plt.plot(range(1, len(history[line2]) + 1), history[line2])
+    plt.xlabel(x_as)
+    plt.ylabel(y_as)
+    plt.title(title)
+    plt.legend([line1, line2])
+    plt.show()
+    
+def load_neural_network(model_path, weights_path):
+    """
+    Load the model.
+    """
+    
+    # Load model
+    model = load_model(model_path)
+    
+    # Load weights
+    model.load_weights(weights_path)
+    
+    # Return model
+    return model
+
+def plot_image_activation(model_path, weights_path, img_path):
+    """
+    This images shows the activation of the last convolutional layer.
+    """
+    
+    # Define model
+    model = load_neural_network(model_path, weights_path)
+    
+    # Extract model_input_shape and relevant information
+    model_input_shape = model.layers[0].input_shape
+    
+    # Define color mode
+    if model_input_shape[-1] == 1:
+        color_mode = 'grayscale'
+        
+    elif model_input_shape[-1] == 3:
+        color_mode = 'rgb'
+    
+    # Extract target size
+    target_size = model.layers[0].input_shape[1:3]
+    
+    # Load the image that we want to plot
+    img = image.load_img(path        = img_path,
+                         color_mode  = color_mode,
+                         target_size = target_size)
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    
+    if model_input_shape[-1] == 3:
+        x = preprocess_input(x)
+        
+    
+    # Make a prediction
+    preds = model.predict(x)
+    
+    # Print predictions
+    print('Predictions: {}.'.format(preds))
+    
+    
+    # Get information of the prediction
+    argmax = np.argmax(preds[0])
+    output = model.output[:, argmax]
+    
+    for layer in model.layers:
+        if 'conv' in layer.name: 
+            last_conv_layer_name = layer.name
+    last_conv_layer = model.get_layer(last_conv_layer_name)
+
+    grads = K.gradients(output, last_conv_layer.output)[0]
+    pooled_grads = K.mean(grads, axis=(0, 1, 2))
+    iterate = K.function([model.input], [pooled_grads, last_conv_layer.output[0]])
+    pooled_grads_value, conv_layer_output_value = iterate([x])
+    
+    for i in range(last_conv_layer.output_shape[-1]):
+        conv_layer_output_value[:, :, i] *= pooled_grads_value[i]
+
+    
+    # Plot the result
+    heatmap = np.mean(conv_layer_output_value, axis=-1)
+    heatmap = np.maximum(heatmap, 0)
+    heatmap /= np.max(heatmap)
+    
+    img = cv2.imread(img_path)
+    heatmap = cv2.resize(heatmap, (img.shape[1], img.shape[0]))
+    heatmap = np.uint8(255 * heatmap)
+    heatmap = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
+    hif = .8
+    superimposed_img = heatmap * hif + img
+
+    output = os.path.join(os.getcwd(), 'test.jpeg')
+    cv2.imwrite(output, superimposed_img)
+
+    img = mpimg.imread(output)
+    
+    plt.imshow(img)
+    plt.axis('off')    
+    
+    # Delete img after showing
+    os.remove(output)
